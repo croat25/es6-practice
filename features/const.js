4953b06d92464c921c350c0a0cf7f18a7b5954d6
Bruno Salapic <sala4460@mylaurier.ca>
2017-01-26 10:04:23 -0500

    added const file

---
+++ features/const.js
@@ -0,0 +1,17 @@
+const PORT=3000;
+//const value cant be changed
+
+//console.log(PORT);
+//PORT=3001;
+//var PORT=3001;
+//console.log(PORT+1);
+
+
+const CONFIG={
+	PORT:3000
+};
+
+console.log(CONFIG);
+
+CONFIG.PORT=3001;
+console.log(CONFIG);
